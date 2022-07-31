1d1d9550783598618c896bc058b6c9518bcdfef1
Samandar <samandarkhodjiyev@gmail.com>
2022-07-31 11:40:44 +0900

    new challenges

--- index.py
+++ index.py
@@ -49,41 +49,103 @@
 
 # Generators taks-2
 
-def square_numbers(arr):
-    for num in arr:
-        yield num*num
+# def square_numbers(arr):
+#     for num in arr:
+#         yield num*num
 
-arr = [1,2,3,4,5,6]
-nums = square_numbers(arr)
+# arr = [1,2,3,4,5,6]
+# nums = square_numbers(arr)
 
-[print(num) for num in nums]
-
-
+# [print(num) for num in nums]
 
 
 
-people = ['S1mple','Ax1le','Hobbit','Nafany']
 
 
-roles = ['AWP','RIFLER','Support','IGL']
+# people = ['S1mple','Ax1le','Hobbit','Nafany']
 
-def gen(people,role):
-    for player,role in zip(people,roles):
-        yield player + " is a " + role
 
-def list_of(people,role):
-    i = 1
-    for player,role in zip(people,roles):
-        person = {
-            'id': i,
-            'name': player,
-            'role': role,
-        }
-        i+=1
-        yield person
+# roles = ['AWP','RIFLER','Support','IGL']
 
-geners = gen(people,roles)
-[print(g) for g in geners]
+# def gen(people,role):
+#     for player,role in zip(people,roles):
+#         yield player + " is a " + role
 
-l = list_of(people,roles)
-[print(g) for g in l]
+# def list_of(people,role):
+#     i = 1
+#     for player,role in zip(people,roles):
+#         person = {
+#             'id': i,
+#             'name': player,
+#             'role': role,
+#         }
+#         i+=1
+#         yield person
+
+# geners = gen(people,roles)
+# [print(g) for g in geners]
+
+# l = list_of(people,roles)
+# [print(g) for g in l]
+
+
+
+
+# #Fizzbuzz problem
+
+# for i in range(1,105):
+#     if i % 5 == 0 and i % 3 == 0:
+#         print(f"{i} is Fizzbuz")
+#     elif i % 3 == 0:
+#          print(f"{i} is Fizz")
+#     elif i % 5 == 0:
+#          print(f"{i} is Buzz")
+#     else:
+#          print(f"{i} is nothing")
+        
+
+# set is a python data type which has no duplicates in it .Even if you insert duplicated while releasing it is not going to return one item two times
+
+
+
+#Fibonacci sequence
+
+# def fibonacci(num):
+#     a,b = 0,1
+#     for i in range(1,num):
+#          a,b = b,a+b
+#          yield a
+         
+         
+
+
+# for item in fibonacci(10):
+#     print(item)
+
+
+
+
+#OOP revision
+# class Person:
+#     def __init__(self,name):
+#         self.name = name 
+
+
+#     def intro(self):
+#         print(self.name)
+
+# class SuperHero(Person):
+#     def __init__(self,name,ability):
+#         super().__init__(name)
+#         self.ability = ability
+
+#     def intro(self):
+#         super().intro()
+#         print(f"... and i am {self.ability}")
+
+
+# person = Person('samandar')
+# hero = SuperHero('samandar','clever')
+
+# person.intro()
+# hero.intro()
